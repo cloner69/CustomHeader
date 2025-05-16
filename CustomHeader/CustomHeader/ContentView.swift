a30a8c624ae82ab7cbb4eced9f3ee91122a67e26
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-05-17 03:45:53 +0700

    header

---
+++ CustomHeader/CustomHeader/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CustomHeader
+//
+//  Created by Adrian Suryo Abiyoga on 02/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+            .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ CustomHeader/CustomHeader/CustomHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomHeaderApp.swift
+//  CustomHeader
+//
+//  Created by Adrian Suryo Abiyoga on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
